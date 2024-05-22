8caba62d326b2469811644db238e235d6ee3da8d
Ahmed Yousry Helal <ahmedu3helal@gmail.com>
2024-05-22 16:44:32 +0300

    Create Code First Models

---
+++ Event-Planinng-System-DAL/Models/Invite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Planinng_System_DAL.Models
+{
+    public class Invite
+    {
+        [ForeignKey("EventNavigation")]
+        public int EventId { get; set; }
+        [ForeignKey("UserNavigation")]
+        public int UserId { get; set; }
+        public bool IsSent { get; set; }
+        public Event EventNavigation { get; set; }
+        public User UserNavigation { get; set; }
+    }
+}
---
+++ Event-Planinng-System-DAL/Models/ToDoList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Planinng_System_DAL.Models
+{
+    public class ToDoList
+    {
+        public DateTime DeadLineTime { get; set; }
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+        [Range(0, 10000000000)]
+        public int? ToDoListBudget { get; set; }
+        [MinLength(3)]
+        public string Description { get; set; }
+        [ForeignKey("EventNavigation")]
+        public int EventId { get; set; }    
+        public Event EventNavigation { get; set; }
+    }
+}
